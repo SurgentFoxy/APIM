f8897bea559646992c225322e95deaf8843cec24
SurgentFoxy <ammarkhan9094@gmail.com>
2019-11-10 16:27:47 +0500

    Initiated the main (Front end) Program of APIM
    No special features added

---
+++ APIM_Main_Repo/APIM_Main_Repo/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APIM_Main_Repo//The main Program(Don't Apply Changes until it's 100 working n checked)
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
