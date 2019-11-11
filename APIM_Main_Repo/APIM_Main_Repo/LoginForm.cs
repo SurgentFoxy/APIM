21f5648ae76387b84c49e58e2ee90d579e9a3b9f
SurgentFoxy <ammarkhan9094@gmail.com>
2019-11-12 01:10:18 +0500

    Added Login Form
    Completed:
    - Buttons and Textboxs added.
    Remaining:
    - No coloring
    - No textures
    - Logo

--- APIM_Main_Repo/APIM_Main_Repo/LoginForm.cs
+++ APIM_Main_Repo/APIM_Main_Repo/LoginForm.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace APIM_Main_Repo//The main Program(Don't Apply Changes until it's 100 working n checked)
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
--- APIM_Main_Repo/APIM_Main_Repo/Program.cs
+++ APIM_Main_Repo/APIM_Main_Repo/Program.cs
@@ -15,7 +15,7 @@ namespace APIM_Main_Repo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
